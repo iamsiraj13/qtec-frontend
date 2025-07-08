14fd5717cdd53fc000c5f801bd46bbccfd6f12d4
iamsiraj13 <iamsiraj13@gmail.com>
2025-07-08 21:35:51 +0600

    update readme

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -35,7 +35,7 @@ function HomePage() {
 
   return (
     <div className="container max-w-7xl mx-auto p-4">
-      <h1 className="text-3xl font-bold mb-6">Products</h1>
+      <h1 className="text-3xl font-bold mb-6">All Products</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-6 gap-6">
         {products.map((product) => (
           <Link
